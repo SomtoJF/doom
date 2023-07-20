03d256d4617f3236895d6107eadf4c6e8f1e99e9
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-07-20 21:04:34 +0100

    Install font-wrap and change theme

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-;;(setq doom-font (font-spec :family "Iosevka SS05" :size 13 :weight 'light)
+;;(setq doom-font (font-spec :family "Iosevka" :size 13 :weight 'light)
 ;;    doom-variable-pitch-font (font-spec :family "SF Pro Text" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-Iosvkem)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
